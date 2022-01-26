a9f3d0571a53174ed6d3770c642d61d5b4442ff0
Denis Fadeev <denis.fadeev@dev.mobileup.ru>
2022-01-26 11:37:20 +0300

    navigation ui

--- app/src/main/java/com/example/scoped_storage_example/navigation/ui/NavigationUi.kt
+++ app/src/main/java/com/example/scoped_storage_example/navigation/ui/NavigationUi.kt
@@ -1,9 +1,20 @@
 package com.example.scoped_storage_example.navigation.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.scoped_storage_example.R
 import com.example.scoped_storage_example.core.ui.theme.AppTheme
 
 @Composable
@@ -11,9 +22,62 @@ fun NavigationUi(
     component: NavigationComponent,
     modifier: Modifier = Modifier
 ) {
-    Text(
-        text = "navigation"
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+            )
+        },
+        content = {
+            NavigationContent(
+                onAppStorageClick = component::onAppStorageClick,
+                modifier = Modifier.padding(it)
+            )
+        }
     )
+}
+
+@Composable
+private fun NavigationContent(
+    onAppStorageClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+        modifier = modifier.padding(top = 20.dp)
+    ) {
+        NavigationButton(
+            text = stringResource(id = R.string.app_storage_title),
+            onClick = onAppStorageClick
+        )
+    }
+}
+
+@Composable
+private fun NavigationButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp)
+    ) {
+        Text(
+            text = text,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
 }
 
 @Preview(showSystemUi = true)
