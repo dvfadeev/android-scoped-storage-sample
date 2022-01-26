da2bb6b3e1f8c5445359b77ec4bf495435a3dd8d
Denis Fadeev <denis.fadeev@dev.mobileup.ru>
2022-01-26 12:29:26 +0300

    edit app theme

--- app/src/main/java/com/example/scoped_storage_example/core/ui/theme/Color.kt
+++ app/src/main/java/com/example/scoped_storage_example/core/ui/theme/Color.kt
@@ -1,8 +1,24 @@
 package com.example.scoped_storage_example.core.ui.theme
 
+import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+object RawColors {
+    val purpleA: Color = Color(0xFF4A148C)
+    val purpleB: Color = Color(0xFF7C43BD)
+    val whiteA: Color = Color(0xFFFFFFFF)
+    val blackA: Color = Color(0xFF000000)
+}
+
+val ColorPalette = lightColors(
+    primary = RawColors.purpleA,
+    primaryVariant = RawColors.purpleB,
+    secondary = RawColors.purpleA,
+    secondaryVariant = RawColors.purpleB,
+    background = RawColors.whiteA,
+    surface = RawColors.whiteA,
+    onPrimary = RawColors.whiteA,
+    onSecondary = RawColors.whiteA,
+    onBackground = RawColors.blackA,
+    onSurface = RawColors.blackA
+)
--- app/src/main/java/com/example/scoped_storage_example/core/ui/theme/Theme.kt
+++ app/src/main/java/com/example/scoped_storage_example/core/ui/theme/Theme.kt
@@ -1,24 +1,10 @@
 package com.example.scoped_storage_example.core.ui.theme
 
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-
-private val ColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200,
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black
-)
 
 @Composable
-fun AppTheme(content: @Composable() () -> Unit) {
+fun AppTheme(content: @Composable () -> Unit) {
     MaterialTheme(
         colors = ColorPalette,
         typography = Typography,
