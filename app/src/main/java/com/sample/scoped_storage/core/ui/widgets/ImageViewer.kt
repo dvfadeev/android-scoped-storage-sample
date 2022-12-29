b354f0fb557f50608ed5b60b116c18b370fc2607
Denis Fadeev <denis.fadeev@gmail.com>
2022-12-29 16:08:08 +0300

    Upgraded libs versions

--- app/src/main/java/com/sample/scoped_storage/core/ui/widgets/ImageViewer.kt
+++ app/src/main/java/com/sample/scoped_storage/core/ui/widgets/ImageViewer.kt
@@ -1,7 +1,6 @@
 package com.sample.scoped_storage.core.ui.widgets
 
 import android.net.Uri
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,12 +10,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
+import coil.compose.AsyncImage
 import coil.decode.VideoFrameDecoder
-import com.sample.scoped_storage.R
+import coil.request.ImageRequest
 import com.sample.scoped_storage.core.ui.theme.additionalColors
 import com.sample.scoped_storage.core.utils.FileTypes
 
@@ -29,35 +27,25 @@ fun ImageViewer(
     isCropEnabled: Boolean = true
 ) {
     if (type.startsWith(FileTypes.MIME_TYPE_IMAGE_ALL) || type.startsWith(FileTypes.MIME_TYPE_VIDEO_ALL)) {
-        val context = LocalContext.current
-
         val sizeModifier = if (isCropEnabled) {
             Modifier.size(size)
         } else {
             Modifier
         }
-
-        Image(
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(uri)
+                .crossfade(true)
+                .decoderFactory(VideoFrameDecoder.Factory())
+                .build(),
+            contentDescription = null,
+            contentScale = if (isCropEnabled) {
+                ContentScale.Crop
+            } else ContentScale.Fit,
             modifier = modifier
                 .then(sizeModifier)
                 .clip(RoundedCornerShape(8.dp))
                 .border(1.dp, MaterialTheme.additionalColors.lightText, RoundedCornerShape(8.dp)),
-            painter = rememberImagePainter(
-                uri,
-                builder = {
-                    with(LocalDensity.current) { size(size.roundToPx()) }
-
-                    placeholder(R.color.cardview_dark_background)
-                    if (type.startsWith(FileTypes.MIME_TYPE_VIDEO_ALL)) {
-                        decoder(VideoFrameDecoder(context))
-                        crossfade(true)
-                    }
-                }
-            ),
-            contentScale = if (isCropEnabled) {
-                ContentScale.Crop
-            } else ContentScale.Fit,
-            contentDescription = null
         )
     }
 }
